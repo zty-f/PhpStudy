11d8dcef6fad2cea6e05cd4a61668460e509ae64
w_zhangtianyong <w_zhangtianyong@xiwang.com>
2024-03-15 10:28:35 +0800

    Test array

--- array/test4.php
+++ array/test4.php
@@ -12,3 +12,24 @@ var_dump($arr['c']);
 var_dump($arr['d']);  //会报错 Undefined index: d 不影响流程执行
 
 var_dump(1111111);
+
+// 实现一个简单的递归
+function test($arr, $key)
+{
+    if (empty($arr[$key])) {
+        return '';
+    }
+    return $arr[$key];
+}
+var_dump(test($arr, '4'));
+
+// 数组的遍历
+$arr = [
+    'a' => 1,
+    'b' => 2,
+    'c' => 3,
+    'd' => 4,
+];
+foreach ($arr as $key => $value){
+    echo $key . '=>' . $value . '</br>';
+}
